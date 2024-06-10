e2b9ca8ffbc69a0acbc9ee91df5760ab54dbba01
P Sai Charan <90904044+saicharan1901@users.noreply.github.com>
2024-06-11 00:13:27 +0530

    created organized folders to work on

---
+++ frontend/app/homeee/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Comp from "./component";
+
+export default function Home1(){
+    return(
+        <div>
+            <Comp /> 
+        </div>
+    );
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import Home1 from "./homeee/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="text-6xl justify-center place-items-center text-red-800 font-extrabold grid">
+      <Link href="/homeee" className="cursor-pointer">
+
+        Hello
+      </Link>
       ProdUp
     </div>
   );
