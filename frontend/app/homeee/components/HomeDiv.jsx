84455b6150c14b947aa6ef46e1bcc65ca7b2fad3
Surya Abhiram <gvnsabhiram@gmail.com>
2024-07-05 17:37:26 +0530

    added background image for home

--- frontend/app/homeee/components/HomeDiv.jsx
+++ frontend/app/homeee/components/HomeDiv.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 const HomeDiv = () => {
   return (
-    <div className="flex items-center justify-center bg-gradient-to-br from-sky-800 to-sky-950 min-h-screen text-white">
-      <div className="text-center p-8 rounded-lg ">
-        <h1 className="text-6xl font-extrabold tracking-tight mb-6">
+    <div
+      className="relative flex items-center justify-end min-h-screen bg-gradient-to-br from-sky-800 to-sky-950 text-white"
+      style={{
+        backgroundImage: `url("/home image.png")`,
+        backgroundSize: '55% auto',
+        backgroundPosition: 'left -10%',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-60 "></div> {/* Overlay */}
+      <div className="relative text-center p-8 rounded-lg z-10 m-8">
+        <h1 className="text-6xl font-extrabold tracking-tight mb-6 animate-pulse text-black">
           Welcome to ProdUp
         </h1>
-        <p className="text-lg text-gray-300 mb-8">
+        <p className="text-lg text-black mb-8 font-extrabold">
           Boost your productivity by scheduling tasks, setting reminders, and more!
         </p>
         <button
-          className="px-8 py-4 bg-gradient-to-r from-pink-500 to-red-500 text-white text-lg rounded-lg shadow-lg hover:scale-105 transform transition-transform hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+          className="px-8 py-4 bg-gradient-to-r from-green-400 to-blue-500 text-white text-lg rounded-lg shadow-lg hover:scale-105 transform transition-transform hover:shadow-2xl focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2"
         >
           Get Started
         </button>
@@ -21,3 +30,4 @@ const HomeDiv = () => {
 };
 
 export default HomeDiv;
+
