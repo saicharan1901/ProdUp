f52a699dd3d0581126437df0f20ba7366ff56c81
P Sai Charan <90904044+saicharan1901@users.noreply.github.com>
2024-07-06 12:33:14 +0530

    added simple backend for test hosting

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
-                href="/"
+                href="/homeee"
                 onClick={() => handleLinkClick('Home')}
                 className={`block py-2 px-3 rounded md:p-0 ${
                   activeLink === 'Home'
