b9eece875639d6ec95990daf16c5ed9b3e2ecd94
P Sai Charan <90904044+saicharan1901@users.noreply.github.com>
2024-07-11 17:53:02 +0530

    Added colors with working backend

--- frontend/app/dashboard/notes/page.jsx
+++ frontend/app/dashboard/notes/page.jsx
@@ -1,14 +1,18 @@
-"use client";
+"use client"
 
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation'; // Import useRouter
+import { useRouter } from 'next/navigation';
 import Navbar from '../../../components/navbar';
-import { onAuthStateChangedListener } from '/app/firebase'; // Adjust path as necessary
+import { onAuthStateChangedListener } from '/app/firebase';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { MdModeEditOutline, MdDelete, MdPostAdd } from "react-icons/md";
+import { MdModeEditOutline, MdDelete, MdPostAdd, MdFormatBold, MdFormatItalic } from "react-icons/md";
 import { GrSave } from "react-icons/gr";
+import ColorizeIcon from '@mui/icons-material/Colorize';
+import TextField from '@mui/material/TextField';
+import CircularProgress from '@mui/material/CircularProgress';
 
+const colors = ['#000000', '#A52A2A', '#FF8C00', '#B8860B', '#556B2F', '#2E8B57', '#4682B4', '#1E90FF', '#8A2BE2', '#DA70D6', '#8B4513', '#708090'];
 
 
 const NoteAdder = () => {
@@ -17,7 +21,8 @@ const NoteAdder = () => {
     const [notes, setNotes] = useState([]);
     const [user, setUser] = useState(null);
     const [editingNote, setEditingNote] = useState(null);
-    const [modalOpen, setModalOpen] = useState(false);
+    const [color, setColor] = useState(null); // No default color initially
+    const [showColorPalette, setShowColorPalette] = useState(false); // State to manage color palette visibility
     const router = useRouter();
 
     useEffect(() => {
@@ -57,13 +62,15 @@ const NoteAdder = () => {
             return;
         }
 
+        const noteColor = color || colors[0]; // Default color if no color is selected
+
         try {
             const response = await fetch('https://produpbackend.vercel.app/addnote', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ user_id: user.uid, title, content }),
+                body: JSON.stringify({ user_id: user.uid, title, content, bgcolor: noteColor }),
             });
 
             if (!response.ok) {
@@ -75,9 +82,10 @@ const NoteAdder = () => {
             setNotes([...notes, newNote]);
             setTitle('');
             setContent('');
+            setColor(null); // Reset color
             toast.success('Note added successfully!');
 
-            window.location.reload('Brother'); // Optional: Reload the page to reflect the new note
+            window.location.reload();
         } catch (error) {
             toast.error('Failed to add note');
             console.error('Error adding note:', error);
@@ -90,13 +98,15 @@ const NoteAdder = () => {
             return;
         }
 
+        const noteColor = color || colors[0]; // Default color if no color is selected
+
         try {
             const response = await fetch(`https://produpbackend.vercel.app/editnote/${editingNote}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ title, content }),
+                body: JSON.stringify({ title, content, bgcolor: noteColor }),
             });
 
             if (!response.ok) {
@@ -104,13 +114,13 @@ const NoteAdder = () => {
                 throw new Error(errorResponse.message || 'Failed to edit note');
             }
 
-            setNotes(notes.map(note => note.note_id === editingNote ? { ...note, title, content } : note));
+            setNotes(notes.map(note => note.note_id === editingNote ? { ...note, title, content, bgcolor: noteColor} : note));
             setTitle('');
             setContent('');
+            setColor(null); // Reset color
             setEditingNote(null);
             toast.success('Note edited successfully!');
-
-            // window.location.reload();
+            window.location.reload();
         } catch (error) {
             toast.error('Failed to edit note');
             console.error('Error editing note:', error);
@@ -140,48 +150,135 @@ const NoteAdder = () => {
     const startEditing = (note) => {
         setTitle(note.title);
         setContent(note.content);
+        setColor(note.color || colors[0]); // Set color to the note's color
         setEditingNote(note.note_id);
     };
 
+    const toggleColorPalette = () => {
+        setShowColorPalette(!showColorPalette);
+    };
+
+    const handleColorSelect = (selectedColor) => {
+        setColor(selectedColor);
+        setShowColorPalette(false); // Hide the color palette after selecting a color
+    };
+
+    const applyFormatting = (format) => {
+        let textarea = document.getElementById('content');
+        let start = textarea.selectionStart;
+        let end = textarea.selectionEnd;
+        let selectedText = content.substring(start, end);
+        let beforeText = content.substring(0, start);
+        let afterText = content.substring(end);
+
+        let formattedText;
+        if (format === 'bold') {
+            formattedText = `**${selectedText}**`;
+        } else if (format === 'italic') {
+            formattedText = `*${selectedText}*`;
+        }
+
+        setContent(beforeText + formattedText + afterText);
+    };
+
+    const renderContent = (text) => {
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        return text.split(urlRegex).map((part, index) =>
+            urlRegex.test(part) ? (
+                <a key={index} href={part} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+                    {part}
+                </a>
+            ) : (
+                part
+            )
+        );
+    };
+
     if (!user) {
-        return <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">Loading...</div>;
+        return <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center"><CircularProgress /></div>;
     }
 
     return (
         <div className="bg-gray-900 text-white min-h-screen font-mono">
             <Navbar />
-            <h1 className='mx-auto justify-center items-center flex text-4xl font-bold text-yellow-700 mt-10 animate-bounce'>Notes!</h1>
             <div className="container mx-auto py-8 w-2/3 lg:w-1/2">
                 <div className="bg-gray-800 rounded-lg shadow-lg px-8 py-6 mb-8 border-gray-600 border mx-auto hover:border-yellow-700 transition duration-300 transform hover:scale-105 hover:shadow-2xl">
                     <div className="mb-4">
-                        <input
-                            type="text"
+                        <TextField
                             id="title"
-                            name="title"
-                            className="w-full px-3 py-2 placeholder-gray-400 rounded-lg focus:outline-none bg-transparent text-white transition duration-300 transform focus:ring-2"
-                            placeholder="Title..."
+                            label="Title"
+                            variant="standard"
+                            fullWidth
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
+                            InputProps={{
+                                className: "text-white",
+                                style: { color: 'white' },
+                            }}
+                            InputLabelProps={{
+                                className: "text-white",
+                                style: { color: 'white' },
+                            }}
                         />
                     </div>
                     <div className="mb-4">
-                        <textarea
+                        <TextField
                             id="content"
-                            name="content"
-                            rows="5"
-                            className="w-full px-3 py-2 placeholder-gray-400 rounded-lg focus:outline-none bg-transparent text-white transition duration-300 transform focus:ring-2"
-                            placeholder="Content..."
+                            label="Content"
+                            variant="standard"
+                            fullWidth
+                            multiline
+                            rows={5}
                             value={content}
                             onChange={(e) => setContent(e.target.value)}
-                            style={{ resize: "none" }}
-                        ></textarea>
+                            InputProps={{
+                                className: "text-white",
+                                style: { color: 'white' },
+                            }}
+                            InputLabelProps={{
+                                className: "text-white",
+                                style: { color: 'white' },
+                            }}
+                        />
                     </div>
-                    <button
-                        className="flex items-center justify-center px-4 py-2 mt-4 text-white bg-yellow-700 rounded-full hover:bg-yellow-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-opacity-50"
-                        onClick={editingNote ? handleEditNote : handleAddNote}
-                    >
-                        {editingNote ? <GrSave /> : <MdPostAdd />}
-                    </button>
+                    <div className="mb-4 flex gap-2 justify-left">
+                        <button
+                            className="flex items-center justify-center px-4 py-2 text-white bg-yellow-700 rounded-full hover:bg-yellow-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-opacity-50"
+                            onClick={editingNote ? handleEditNote : handleAddNote}
+                        >
+                            {editingNote ? <GrSave size={20} /> : <MdPostAdd size={20} />}
+                        </button>
+                        <button
+                            className="flex items-center justify-center px-4 py-2 text-white bg-yellow-700 rounded-full hover:bg-yellow-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-opacity-50"
+                            onClick={toggleColorPalette}
+                        >
+                            <ColorizeIcon fontSize="small" />
+                        </button>
+                        {/* <button
+                            className="flex items-center justify-center px-4 py-2 text-white bg-yellow-700 rounded-full hover:bg-yellow-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-opacity-50"
+                            onClick={() => applyFormatting('bold')}
+                        >
+                            <MdFormatBold size={20} />
+                        </button>
+                        <button
+                            className="flex items-center justify-center px-4 py-2 text-white bg-yellow-700 rounded-full hover:bg-yellow-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-opacity-50"
+                            onClick={() => applyFormatting('italic')}
+                        >
+                            <MdFormatItalic size={20} />
+                        </button> */}
+                    </div>
+                    {showColorPalette && (
+                        <div className="flex flex-wrap gap-2 mt-4 justify-end">
+                            {colors.map((c) => (
+                                <button
+                                    key={c}
+                                    className={`w-8 h-8 rounded-full ${c === color ? 'ring-2 ring-yellow-700' : ''}`}
+                                    style={{ backgroundColor: c }}
+                                    onClick={() => handleColorSelect(c)}
+                                />
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
 
@@ -190,15 +287,16 @@ const NoteAdder = () => {
                     <div
                         key={note.note_id}
                         className="bg-gray-900 rounded-lg shadow-lg p-6 mb-6 break-inside-avoid break-words hover:border-yellow-700 border transition-all duration-300 ease-in-out transform hover:scale-105 relative"
+                        style={{ backgroundColor: note.color || 'transparent' }} // Apply the note color
                     >
-                        <h3 className="font-mono font-extrabold text-white">{note.title}</h3>
-                        <p className="text-gray-300">{note.content}</p>
+                        <h3 className="font-extrabold text-white text-xl">{note.title}</h3>
+                        <p className="text-white text-xs">{renderContent(note.content)}</p>
                         <br />
 
-                        <div className=' relative bottom-0 left-0 flex flex-row gap-5'>
+                        <div className='relative bottom-0 left-0 flex flex-row gap-5'>
                             <div>
                                 <button
-                                    className=" p-1 text-white hover:text-yellow-600 transition-all duration-300 ease-in-out opacity-100 group-hover:opacity-100"
+                                    className="p-1 text-white hover:text-yellow-600 transition-all duration-300 ease-in-out opacity-100 group-hover:opacity-100"
                                     onClick={() => startEditing(note)}
                                 >
                                     <MdModeEditOutline />
@@ -206,7 +304,7 @@ const NoteAdder = () => {
                             </div>
                             <div>
                                 <button
-                                    className=" p-1 text-white hover:text-red-600 transition-all duration-300 ease-in-out opacity-100 group-hover:opacity-100"
+                                    className="p-1 text-white hover:text-red-600 transition-all duration-300 ease-in-out opacity-100 group-hover:opacity-100"
                                     onClick={() => handleDelete(note.note_id)}
                                 >
                                     <MdDelete />
